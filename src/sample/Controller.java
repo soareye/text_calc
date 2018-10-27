fb904a63863201dab773feb86b7122a9c6f9d276
Erik Snilsberg <eriksnils@outlook.com>
2018-10-27 21:44:32 +0200

    Basic GUI with css.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML private TextField inputField;
+    @FXML private Text result;
+
+    @FXML
+    private void calculate() {
+        String input = inputField.getText();
+        String calcResult = String.valueOf(MathExpParser.calculate(input));
+        inputField.setText("");
+        result.setText(input + " = " + calcResult);
+    }
 }
