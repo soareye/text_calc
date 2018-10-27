4c27a3695cfac56168dfb1be996e7067ff8dd86d
Erik Snilsberg <eriksnils@outlook.com>
2018-10-27 21:59:24 +0200

    Fixed an error where the caveat would calculate the power of the entire product to it's left. It now works as intented, only calculating a power of the argument immediately to its left.

--- src/sample/MathExpParser.java
+++ src/sample/MathExpParser.java
@@ -1,46 +1,18 @@
 package sample;
 
+import java.util.Arrays;
 import java.util.function.UnaryOperator;
 
 public class MathExpParser {
 
     private static String curOp = "";
 
-    public static void main(String args[]) {
-
-        System.out.println(calculate("1000*asin(sin(1))/1000*1000+(100+(100+500+(((100)+100)+100)))"));
-        System.out.println();
-
-        System.out.println(calculate("sin(" + String.valueOf(Math.PI) + ")"));
-        System.out.println();
-
-        System.out.println(calculate("5 + 5 + (10 - 5)/(2 + 3)"));
-    }
-
-    public static boolean valid(String expression) {
-        return true;
-    }
-
     public static double calculate(String expression) {
-
-        String splitExp[] = expression.split("");
-
-        int whitespacecount = 0;
-        for (int i = 0; i < splitExp.length; i++) {
-            if (splitExp[i].matches("\\s")) {
-                whitespacecount++;
-            }
-        }
-
-        String symbols[] = new String[splitExp.length - whitespacecount];
-
-        int j = 0;
-        for (int i = 0; i < splitExp.length; i++) {
-            if (!splitExp[i].matches("\\s")) {
-                symbols[j] = splitExp[i];
-                j++;
-            }
-        }
+        curOp = "";
+        // Remove whitespace and put individual chars in a String array:
+        String symbols[] = Arrays.stream(expression.split(""))
+                .filter(x->!x.matches("\\s"))
+                .toArray(String[]::new);
 
         return calcIter(symbols, 0);
     }
@@ -50,7 +22,9 @@ public class MathExpParser {
         double result = 0;
         double product = 0;
         double curNum = 0;
+        double prevNum = 0;
         String prevSym = "";
+        boolean power = false;
 
         StringBuilder numStrBuilder = new StringBuilder();
 
@@ -61,6 +35,11 @@ public class MathExpParser {
                 curNum = Double.parseDouble(numStrBuilder.toString());
 
             } else if (symbols[i].matches("[*/+\\-]")) {
+                if (power) {
+                    curNum = Math.pow(prevNum, curNum);
+                    power = false;
+                }
+
                 product = binaryOps(prevSym, product, curNum);
                 numStrBuilder = new StringBuilder();
                 prevSym = symbols[i];
@@ -70,42 +49,71 @@ public class MathExpParser {
                     product = 0;
                 }
 
+            } else if (symbols[i].matches("\\^")) {
+                prevNum = curNum;
+                power = true;
+                numStrBuilder = new StringBuilder();
+
             } else if (symbols[i].matches("\\(")) {
                 curNum = calcIter(symbols, i + 1);
-
-                i++;
-                i = traverseParentheses(symbols, i);
+                i = traverseParentheses(symbols, i + 1);
 
             } else if (symbols[i].matches("\\)")) {
                 product = binaryOps(prevSym, product, curNum);
                 return result + product;
 
-            } else {
-
+            } else if (matchesUnaryOps(symbols, i)) {
                 curNum = additionalUnaryOps(symbols, i);
+                i = traverseParentheses(symbols, i + curOp.length() + 1);
 
-                i += (curOp.length() + 1);
-                i = traverseParentheses(symbols, i);
+            } else {
+                throw new IllegalArgumentException("Undefined mathematical expression!");
             }
         }
 
+        // Finish up by combining product and curNum, adding it to result and returning:
+        if (power) curNum = Math.pow(prevNum, curNum);
         product = binaryOps(prevSym, product, curNum);
         return result + product;
     }
 
-    public static double binaryOps(String op, double product, double curNum) {
+    public static double binaryOps(String op, double firstArg, double secArg) {
         if (op.matches("-")) {
-            return -curNum;
+            return -secArg;
 
         } else if (op.matches("\\*")) {
-            return product * curNum;
+            return firstArg * secArg;
 
         } else if (op.matches("/")) {
-            return product / curNum;
+            return firstArg / secArg;
 
         } else {
-            return curNum;
+            return secArg;
         }
+    }
+
+    public static boolean matchesUnaryOps(String symbols[], int index) {
+        String opNames[] = {"sin", "cos", "tan", "asin", "acos", "atan"};
+        String longest = Arrays.stream(opNames)
+                .reduce("", (x, y)-> x.length() > y.length() ? x : y);
+
+        if (symbols.length - index < longest.length())
+            return false;
+
+        for (String opName : opNames) {
+            String opNameChars[] = opName.split("");
+            boolean isOp = true;
+
+            for (int i = 0; i < opNameChars.length; i++) {
+                if (!symbols[index + i].matches(opNameChars[i]))
+                    isOp = false;
+            }
+
+            if (isOp)
+                return true;
+        }
+
+        return false;
     }
 
     public static double additionalUnaryOps(String symbols[], int index) {
@@ -149,7 +157,6 @@ public class MathExpParser {
     }
 
     public static double unaryPostfixOps(String nextSym, double curNum) {
-
         if (nextSym.matches("!"))
             return factorial(curNum);
 
